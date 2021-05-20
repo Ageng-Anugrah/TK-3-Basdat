2e1b9c6915f78f89257af449b8ae7d1b3c4a205d
Ageng Anugrah Wardoyo Putra <agenganugrah@gmail.com>
2021-05-20 20:14:39 +0700

    Add trigger tg op

--- Trigger.sql
+++ Trigger.sql
@@ -1,5 +1,4 @@
 -- 1 --
-
 CREATE OR REPLACE FUNCTION CHECK_PASSWORD() RETURNS TRIGGER AS
 $$
 BEGIN
@@ -25,24 +24,40 @@ $$
 DECLARE 
     harga integer;
 BEGIN
-    SELECT PM.harga into harga
-    FROM PAKET_MAKAN PM
-    WHERE NEW.kodehotel = PM.kodehotel AND NEW.kodepaket = PM.kodepaket
+    IF (TG_OP = 'INSERT') THEN
+        SELECT PM.harga into harga
+        FROM PAKET_MAKAN PM
+        WHERE NEW.kodehotel = PM.kodehotel AND NEW.kodepaket = PM.kodepaket
 
-    UPDATE TRANSAKSI_MAKAN TM
-    SET totalbayar = totalbayar + harga
-    WHERE NEW.id_transaksi = TM.idTransaksi and NEW.IdTransaksiMakan = TM.IdTransaksiMakan;
+        UPDATE TRANSAKSI_MAKAN TM
+        SET totalbayar = totalbayar + harga
+        WHERE NEW.id_transaksi = TM.idTransaksi AND NEW.IdTransaksiMakan = TM.IdTransaksiMakan;
 
-    UPDATE TRANSAKSI_HOTEL TH
-    SET totalbayar = totalbayar + harga
-    WHERE NEW.id_transaksi = TH.idTransaksi;
-    RETURN NEW;
+        UPDATE TRANSAKSI_HOTEL TH
+        SET totalbayar = totalbayar + harga
+        WHERE NEW.id_transaksi = TH.idTransaksi;
+        RETURN NEW;
+
+    ELSIF (TG_OP = 'DELETE') THEN
+        SELECT PM.harga into harga
+        FROM PAKET_MAKAN PM
+        WHERE OLD.kodehotel = PM.kodehotel AND OLD.kodepaket = PM.kodepaket
+
+        UPDATE TRANSAKSI_MAKAN TM
+        SET totalbayar = totalbayar - harga
+        WHERE OLD.id_transaksi = TM.idTransaksi AND OLD.IdTransaksiMakan = TM.IdTransaksiMakan;
+
+        UPDATE TRANSAKSI_HOTEL TH
+        SET totalbayar = totalbayar - harga
+        WHERE OLD.id_transaksi = TH.idTransaksi;
+        RETURN OLD;
+    END IF;
 END 
 $$ 
 LANGUAGE PLPGSQL;
 
 CREATE TRIGGER add_nominal_transaksi_hotel
-AFTER INSERT ON DAFTAR_PESAN
+AFTER INSERT OR DELETE ON DAFTAR_PESAN
 FOR EACH ROW 
 EXECUTE PROCEDURE add_nominal_transaksi_hotel();
 
